10583659e0e86da5072ed221e9c7d403fda6dc8f
Abel Hubena <abeltsegaye@google.com>
2024-01-11 12:21:12 +0000

    Fix Talkback announce "Delete data entry" the accessibility labels are not clear enough in See all entries page.
    Bug: 287733584
    Test: atest
    Change-Id: I7fc86aebf115dba420b5b8ab6084d4f6d41b29de

--- apk/src/com/android/healthconnect/controller/dataentries/ExerciseSessionItemViewBinder.kt
+++ apk/src/com/android/healthconnect/controller/dataentries/ExerciseSessionItemViewBinder.kt
@@ -75,6 +75,9 @@ class ExerciseSessionItemViewBinder(
             mapView.setRoute(data.route)
         }
 
+        deleteButton.contentDescription =
+            view.resources.getString(
+                R.string.data_point_action_content_description, title.contentDescription)
         deleteButton.setOnClickListener {
             logger.logInteraction(DataEntriesElement.DATA_ENTRY_DELETE_BUTTON)
             onDeleteEntryClicked?.onDeleteEntry(data.uuid, data.dataType, index)
