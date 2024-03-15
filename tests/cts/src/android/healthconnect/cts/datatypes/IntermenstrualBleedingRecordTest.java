bee233e2f65be7b3618bbe629c172b6dd9afe82f
Tara Ojo <taraojo@google.com>
2024-03-15 12:59:21 +0000

    Use response from calls to insertRecords in tests (datatypes E-O)
    Most tests assert the initial record passed into insertRecords.
    Since we mutate the inserted records to add the uuid this
    currently works, but in a follow up change we'll update the
    insertRecords API so it no longer mutates the metadata. This
    requires changes to most of the cts tests.
    
    Bug: 314128192
    Test: atest
    Change-Id: Ia411a23238dae74b2b9ffaa1ca70b7b39d03ef30

--- tests/cts/src/android/healthconnect/cts/datatypes/IntermenstrualBleedingRecordTest.java
+++ tests/cts/src/android/healthconnect/cts/datatypes/IntermenstrualBleedingRecordTest.java
@@ -137,8 +137,10 @@ public class IntermenstrualBleedingRecordTest {
                         new ReadRecordsRequestUsingFilters.Builder<>(
                                         IntermenstrualBleedingRecord.class)
                                 .build());
-        IntermenstrualBleedingRecord testRecord = getCompleteIntermenstrualBleedingRecord();
-        TestUtils.insertRecords(Collections.singletonList(testRecord));
+
+        IntermenstrualBleedingRecord testRecord =
+                (IntermenstrualBleedingRecord)
+                        TestUtils.insertRecord(getCompleteIntermenstrualBleedingRecord());
         List<IntermenstrualBleedingRecord> newIntermenstrualBleedingRecords =
                 TestUtils.readRecords(
                         new ReadRecordsRequestUsingFilters.Builder<>(
@@ -161,8 +163,10 @@ public class IntermenstrualBleedingRecordTest {
                         .setStartTime(Instant.now())
                         .setEndTime(Instant.now().plusMillis(3000))
                         .build();
-        IntermenstrualBleedingRecord testRecord = getCompleteIntermenstrualBleedingRecord();
-        TestUtils.insertRecords(Collections.singletonList(testRecord));
+
+        IntermenstrualBleedingRecord testRecord =
+                (IntermenstrualBleedingRecord)
+                        TestUtils.insertRecord(getCompleteIntermenstrualBleedingRecord());
         List<IntermenstrualBleedingRecord> newIntermenstrualBleedingRecords =
                 TestUtils.readRecords(
                         new ReadRecordsRequestUsingFilters.Builder<>(
@@ -190,8 +194,10 @@ public class IntermenstrualBleedingRecordTest {
                                                 .setPackageName(context.getPackageName())
                                                 .build())
                                 .build());
-        IntermenstrualBleedingRecord testRecord = getCompleteIntermenstrualBleedingRecord();
-        TestUtils.insertRecords(Collections.singletonList(testRecord));
+
+        IntermenstrualBleedingRecord testRecord =
+                (IntermenstrualBleedingRecord)
+                        TestUtils.insertRecord(getCompleteIntermenstrualBleedingRecord());
         List<IntermenstrualBleedingRecord> newIntermenstrualBleedingRecords =
                 TestUtils.readRecords(
                         new ReadRecordsRequestUsingFilters.Builder<>(
@@ -252,10 +258,10 @@ public class IntermenstrualBleedingRecordTest {
     public void testDeleteIntermenstrualBleedingRecord_recordId_filters()
             throws InterruptedException {
         List<Record> records =
-                List.of(
-                        getBaseIntermenstrualBleedingRecord(),
-                        getCompleteIntermenstrualBleedingRecord());
-        TestUtils.insertRecords(records);
+                TestUtils.insertRecords(
+                        List.of(
+                                getBaseIntermenstrualBleedingRecord(),
+                                getCompleteIntermenstrualBleedingRecord()));
 
         for (Record record : records) {
             TestUtils.verifyDeleteRecords(
@@ -295,12 +301,12 @@ public class IntermenstrualBleedingRecordTest {
     @Test
     public void testDeleteIntermenstrualBleedingRecord_usingIds() throws InterruptedException {
         List<Record> records =
-                List.of(
-                        getBaseIntermenstrualBleedingRecord(),
-                        getCompleteIntermenstrualBleedingRecord());
-        List<Record> insertedRecord = TestUtils.insertRecords(records);
+                TestUtils.insertRecords(
+                        List.of(
+                                getBaseIntermenstrualBleedingRecord(),
+                                getCompleteIntermenstrualBleedingRecord()));
         List<RecordIdFilter> recordIds = new ArrayList<>(records.size());
-        for (Record record : insertedRecord) {
+        for (Record record : records) {
             recordIds.add(RecordIdFilter.fromId(record.getClass(), record.getMetadata().getId()));
         }
 
@@ -392,8 +398,8 @@ public class IntermenstrualBleedingRecordTest {
         assertThat(response.getDeletedLogs().size()).isEqualTo(0);
 
         List<Record> testRecord =
-                Collections.singletonList(getCompleteIntermenstrualBleedingRecord());
-        TestUtils.insertRecords(testRecord);
+                TestUtils.insertRecords(
+                        Collections.singletonList(getCompleteIntermenstrualBleedingRecord()));
         response = TestUtils.getChangeLogs(changeLogsRequest);
         assertThat(response.getUpsertedRecords().size()).isEqualTo(1);
         assertThat(
@@ -549,7 +555,7 @@ public class IntermenstrualBleedingRecordTest {
         assertThat(requestUsingIds.getRecordIdFilters()).isNotNull();
         List<IntermenstrualBleedingRecord> result = TestUtils.readRecords(requestUsingIds);
         assertThat(result).hasSize(insertedRecords.size());
-        assertThat(result).containsExactlyElementsIn(recordList);
+        assertThat(result).containsExactlyElementsIn(insertedRecords);
     }
 
     IntermenstrualBleedingRecord getIntermenstrualBleedingRecord_update(
--- tests/cts/src/android/healthconnect/cts/datatypes/OvulationTestRecordTest.java
+++ tests/cts/src/android/healthconnect/cts/datatypes/OvulationTestRecordTest.java
@@ -125,8 +125,9 @@ public class OvulationTestRecordTest {
                 TestUtils.readRecords(
                         new ReadRecordsRequestUsingFilters.Builder<>(OvulationTestRecord.class)
                                 .build());
-        OvulationTestRecord testRecord = getCompleteOvulationTestRecord();
-        TestUtils.insertRecords(Collections.singletonList(testRecord));
+
+        OvulationTestRecord testRecord =
+                (OvulationTestRecord) TestUtils.insertRecord(getCompleteOvulationTestRecord());
         List<OvulationTestRecord> newOvulationTestRecords =
                 TestUtils.readRecords(
                         new ReadRecordsRequestUsingFilters.Builder<>(OvulationTestRecord.class)
@@ -146,8 +147,8 @@ public class OvulationTestRecordTest {
                         .setStartTime(Instant.now())
                         .setEndTime(Instant.now().plusMillis(3000))
                         .build();
-        OvulationTestRecord testRecord = getCompleteOvulationTestRecord();
-        TestUtils.insertRecords(Collections.singletonList(testRecord));
+        OvulationTestRecord testRecord =
+                (OvulationTestRecord) TestUtils.insertRecord(getCompleteOvulationTestRecord());
         List<OvulationTestRecord> newOvulationTestRecords =
                 TestUtils.readRecords(
                         new ReadRecordsRequestUsingFilters.Builder<>(OvulationTestRecord.class)
@@ -173,8 +174,8 @@ public class OvulationTestRecordTest {
                                                 .setPackageName(context.getPackageName())
                                                 .build())
                                 .build());
-        OvulationTestRecord testRecord = getCompleteOvulationTestRecord();
-        TestUtils.insertRecords(Collections.singletonList(testRecord));
+        OvulationTestRecord testRecord =
+                (OvulationTestRecord) TestUtils.insertRecord(getCompleteOvulationTestRecord());
         List<OvulationTestRecord> newOvulationTestRecords =
                 TestUtils.readRecords(
                         new ReadRecordsRequestUsingFilters.Builder<>(OvulationTestRecord.class)
@@ -256,8 +257,8 @@ public class OvulationTestRecordTest {
     @Test
     public void testDeleteOvulationTestRecord_recordId_filters() throws InterruptedException {
         List<Record> records =
-                List.of(getBaseOvulationTestRecord(), getCompleteOvulationTestRecord());
-        TestUtils.insertRecords(records);
+                TestUtils.insertRecords(
+                        List.of(getBaseOvulationTestRecord(), getCompleteOvulationTestRecord()));
 
         for (Record record : records) {
             TestUtils.verifyDeleteRecords(
@@ -304,7 +305,7 @@ public class OvulationTestRecordTest {
         }
 
         TestUtils.verifyDeleteRecords(recordIds);
-        for (Record record : records) {
+        for (Record record : insertedRecord) {
             TestUtils.assertRecordNotFound(record.getMetadata().getId(), record.getClass());
         }
     }
@@ -468,8 +469,9 @@ public class OvulationTestRecordTest {
         assertThat(response.getUpsertedRecords().size()).isEqualTo(0);
         assertThat(response.getDeletedLogs().size()).isEqualTo(0);
 
-        List<Record> testRecord = Collections.singletonList(getCompleteOvulationTestRecord());
-        TestUtils.insertRecords(testRecord);
+        List<Record> testRecord =
+                TestUtils.insertRecords(
+                        Collections.singletonList(getCompleteOvulationTestRecord()));
         response = TestUtils.getChangeLogs(changeLogsRequest);
         assertThat(response.getUpsertedRecords().size()).isEqualTo(1);
         assertThat(
