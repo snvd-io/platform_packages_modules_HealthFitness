11898c9b9b63aff1659a2962e2690f17f754b664
Matin Akhlaghinia <matinak@google.com>
2024-05-20 14:06:02 +0000

    Add a button to load FHIR json files from the assets folder.
    At the moment, pressing the button only reads the JSON files. Once
    the write API is ready, we will update this to write the data into HC
    database.
    
    Test: manual
    Bug: 340834861
    Flag: NONE testapp
    Change-Id: I0a3b8ecff6d6f3ffc798449abd0208e51d05caa3

--- testapps/toolbox/src/com/android/healthconnect/testapps/toolbox/ui/PhrOptionsFragment.kt
+++ testapps/toolbox/src/com/android/healthconnect/testapps/toolbox/ui/PhrOptionsFragment.kt
@@ -16,6 +16,7 @@
 
 package com.android.healthconnect.testapps.toolbox.ui
 
+import android.content.Context
 import android.health.connect.HealthConnectManager
 import android.health.connect.MedicalIdFilter
 import android.health.connect.datatypes.MedicalResource
@@ -23,12 +24,15 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.os.asOutcomeReceiver
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.android.healthconnect.testapps.toolbox.R
 import com.android.healthconnect.testapps.toolbox.utils.GeneralUtils.Companion.requireSystemService
 import com.android.healthconnect.testapps.toolbox.utils.GeneralUtils.Companion.showMessageDialog
+import java.io.IOException
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
 
@@ -43,6 +47,10 @@ class PhrOptionsFragment : Fragment(R.layout.fragment_phr_options) {
 
         view.requireViewById<Button>(R.id.phr_read_immunizations_button).setOnClickListener {
             executeAndShowMessage { readAllMedicalResources() }
+        }
+
+        view.requireViewById<Button>(R.id.phr_seed_fhir_jsons_button).setOnClickListener {
+            loadAllFhirJSONs()
         }
     }
 
@@ -72,5 +80,64 @@ class PhrOptionsFragment : Fragment(R.layout.fragment_phr_options) {
             }
         Log.d("READ_MEDICAL_RESOURCES", "Read ${resources.size} resources")
         return resources
+    }
+
+    private fun loadAllFhirJSONs() {
+        val jsonFiles = listFhirJSONFiles(requireContext())
+        if (jsonFiles == null) {
+            Log.e("loadAllFhirJSONs", "No JSON files were found.")
+            Toast.makeText(
+                context, "No JSON files were found.", Toast.LENGTH_SHORT)
+                    .show()
+            return
+        }
+
+        for (jsonFile in jsonFiles) {
+            if (!jsonFile.endsWith(".json")) {
+                continue
+            }
+            val jsonString = loadJSONFromAsset(requireContext(), jsonFile)
+            if (jsonString != null) {
+                Log.i("loadAllFhirJSONs", "$jsonFile: $jsonString")
+            }
+        }
+        showLoadJSONDataDialog()
+    }
+
+    private fun listFhirJSONFiles(context: Context, path: String = ""): List<String>? {
+        val assetManager = context.assets
+        return try {
+            assetManager.list(path)?.toList() ?: emptyList()
+        } catch (e: IOException) {
+            Log.e("listFhirJSONFiles", "Error listing assets in path $path: $e")
+            Toast.makeText(
+                context, "Error listing JSON files: ${e.localizedMessage}", Toast.LENGTH_SHORT)
+                    .show()
+            null
+        }
+    }
+
+    fun loadJSONFromAsset(context: Context, fileName: String): String? {
+        return try {
+            val inputStream = context.assets.open(fileName)
+            val buffer = ByteArray(inputStream.available())
+            inputStream.read(buffer)
+            inputStream.close()
+            buffer.toString(Charsets.UTF_8)
+        } catch (e: IOException) {
+            Log.e("loadJSONFromAsset", "Error reading JSON file: $e")
+            Toast.makeText(
+                context, "Error reading JSON file: ${e.localizedMessage}", Toast.LENGTH_SHORT)
+                    .show()
+            null
+        }
+    }
+
+    private fun showLoadJSONDataDialog() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("FHIR JSON files loaded.")
+        builder.setPositiveButton(android.R.string.ok) { _, _ -> }
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
     }
 }
