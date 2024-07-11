a591d56ccf55c63b222592d51f9470300287b465
Teo Georgescu <teog@google.com>
2024-07-11 17:45:12 +0100

    Home screen changes for the new information architecture: - Adds telemetry - Removes intro text from home page for new IA - Renames "Data and access" button to "Browse data" on the home screen for the new IA
    Flag: com.android.healthfitness.flags.new_information_architecture
    Bug: 352023091
    Test: atest
    Change-Id: If7fc064e15e5aa9f68e3ddd4ff218553e7fde15d

--- apk/src/com/android/healthconnect/controller/utils/FeatureUtils.kt
+++ apk/src/com/android/healthconnect/controller/utils/FeatureUtils.kt
@@ -2,6 +2,7 @@ package com.android.healthconnect.controller.utils
 
 import android.content.Context
 import android.provider.DeviceConfig
+import android.util.Log
 import com.android.healthfitness.flags.Flags
 import dagger.Module
 import dagger.Provides
@@ -62,6 +63,7 @@ class FeatureUtilsImpl(context: Context) : FeatureUtils, DeviceConfig.OnProperti
 
     override fun isNewInformationArchitectureEnabled(): Boolean {
         synchronized(lock) {
+            Log.i("TEOG_NEW_IA", "isNewInformationArch = $isNewInformationArchitectureEnabled")
             return isNewInformationArchitectureEnabled
         }
     }
